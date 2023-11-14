a25b7e209f995e051a3fd4a0687143dc79052164
Hossain <arafat.kd99@gmail.com>
2023-11-14 14:32:00 +0100

    Add Details components

---
+++ src/components/Details.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+const Card = ({ label, value }) => (
+  <li className="">
+    <div className="">{label}</div>
+    <div className="">{value}</div>
+  </li>
+);
+
+const Details = () => {
+  const params = useParams();
+  const { subregion } = params;
+  const countries = useSelector((state) => state.countries.countries);
+
+  const selectedCountry = countries.find((country) => country.name === subregion);
+
+  return (
+    <div>
+      <div className="">
+        <div className="">
+          <div>
+            <img src={selectedCountry?.flag} alt={subregion} className="" />
+          </div>
+        </div>
+        <div className="">
+          <h1 className="">{subregion?.toUpperCase()}</h1>
+          <h3 className="">{`Population: ${selectedCountry?.population}`}</h3>
+        </div>
+      </div>
+      <div className="">STATS BY COUNTRY</div>
+      <div>
+        <ul>
+          <Card label="CAPITAL" value={selectedCountry?.capital} />
+          <Card label="POPULATION" value={selectedCountry?.population} />
+          <Card label="AREA" value={selectedCountry?.area} />
+          <Card label="CURRENCY" value={selectedCountry?.currencies[0]?.code} />
+          <Card label="CALLING CODE" value={selectedCountry?.callingCodes} />
+          <Card label="TIMEZONES" value={selectedCountry?.timezones} />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+export default Details;
