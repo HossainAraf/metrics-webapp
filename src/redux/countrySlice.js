a435feaf853423f9e6d3f4d5d4081b948f4da1df
Hossain <arafat.kd99@gmail.com>
2023-11-14 13:33:34 +0100

    Fetch API data with Thunk

--- src/redux/countrySlice.js
+++ src/redux/countrySlice.js
@@ -7,3 +7,17 @@ const initialState = {
   status: 'idle',
   error: null,
 };
+
+// THUNKS
+export const fetchCountries = createAsyncThunk('countries/fetchCountries', async () => {
+  try {
+    const response = await fetch('https://restcountries.com/v2/region/asia');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.status} - ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
+});
