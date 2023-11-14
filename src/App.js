807ae7d8518f0ef2b0387c1fb777a76996d72508
Hossain <arafat.kd99@gmail.com>
2023-11-14 13:18:48 +0100

    Add routers to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Details from './components/Details';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Details />} />
+      </Routes>
+    </Router>
   );
 }
 
