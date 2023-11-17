64715e262b9e99f291a9ce7fb6e94fdb0a077c7c
Hossain <arafat.kd99@gmail.com>
2023-11-17 19:30:47 +0100

    Update CountryFilter component to accept a search term

--- src/components/CountryFilter.jsx
+++ src/components/CountryFilter.jsx
@@ -4,18 +4,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchCountries } from '../redux/countrySlice';
 
 // COMPONENTS
-const CountryFilter = () => {
+const CountryFilter = (searchTerm) => {
   const dispatch = useDispatch();
-
-  // SELECT STATE FROM REDUX STORE
   const countries = useSelector((state) => state.countries.countries);
-  // USE USEEFEECT TO DISPATCH FETCHCOUNTRIES ACTION
+
   useEffect(() => {
     dispatch(fetchCountries());
   }, [dispatch]);
 
-  // FILTER COUNTRIES BY REGION
-  const filteredCountries = countries.filter((country) => country.region === 'Asia');
+  const filteredCountries = countries.filter((country) => country.region === 'Asia'
+  && country.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return filteredCountries;
 };
 
