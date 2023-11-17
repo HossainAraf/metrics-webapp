49e0a4464f330a68c2aebf232b993ff7073e7ef4
Hossain <arafat.kd99@gmail.com>
2023-11-17 20:04:17 +0100

    Update home.test.js to comply with the change

--- src/_tests_/home.test.js
+++ src/_tests_/home.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'; // Import the Provider
 import store from '../redux/store';
 import Home from '../components/Home';
 
-describe('Home Component Snapshot', () => {
-  it('should match the snapshot', () => {
-    const component = renderer.create(
-      <Provider store={store}>
+test('renders Home component', () => {
+  render(
+    <Provider store={store}>
+      {/* Wrap component with Provider */}
+      <Router>
         <Home />
-      </Provider>,
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+      </Router>
+    </Provider>,
+  );
 });
